a59cd12337d1ec6480874295f3652682f2ead17a
LITH002 <liteenilanjith@gmail.com>
2024-10-02 14:59:14 +0530

    Comment added

--- app/src/main/java/com/example/mycookbook/MainActivity.java
+++ app/src/main/java/com/example/mycookbook/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); //This is a saved instance
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
